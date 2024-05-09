570be50ed768bc7e9fb82e79800b5c9ca4e76185
Sean <seangerardwhite@gmail.com>
2024-05-09 17:41:54 +0100

    Committed all initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project containing the code for creating a model to predict next day closing stock price',
+    author='Sean',
+    license='',
+)
