07e1c8065746de966d205dd39b79d20022581f21
Sean <seangerardwhite@gmail.com>
2024-05-10 15:50:48 +0100

    Improved functionality of class

--- src/data/data_preprocessing.py
+++ src/data/data_preprocessing.py
@@ -16,27 +16,33 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.preprocessing import MinMaxScaler
 
 
-df = pd.read_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/Astrazeneca Price History.xlsx')
+data = pd.read_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/Astrazeneca Price History.xlsx')
 
 
 class CreateData:
-    def __init__(self):
-        pass
+    def __init__(self, df, keywords, timeframe, geo):
+        self.df = df
+        self.keywords = keywords
+        self.timeframe = timeframe
+        self.geo = geo
+        self.trends = None
 
-    def format_data(self, df):
-        df = df.dropna()
-        df['Next Day Close'] = df['Close'].shift(-1)
-        df = df[:-1]
+    def format_data(self):
+        self.df = self.df.dropna()
+        self.df['Next Day Close'] = self.df['Close'].shift(-1)
+        self.df = self.df.iloc[:-1]
 
-        numeric_cols = df.select_dtypes(include=np.number).columns.tolist()
+        numeric_cols = self.df.select_dtypes(include=np.number).columns.tolist()
         numeric_cols.remove('Next Day Close')
-        scaler = MinMaxScaler().fit(df[numeric_cols])
-        df_scaled = scaler.transform(df[numeric_cols])
-        df[numeric_cols] = df_scaled
+        scaler = MinMaxScaler().fit(self.df[numeric_cols])
+        df_scaled = scaler.transform(self.df[numeric_cols])
+        self.df[numeric_cols] = df_scaled
 
-        return df
+        return self.df
 
-    def get_trends(self, keywords, timeframe, geo):
+    def get_trends(self):
+        self.keywords = [i.title() for i in self.keywords]
+
         vader_lexicon_path = '/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/vader_lexicon.txt'
         sia = SentimentIntensityAnalyzer(
             lexicon_file=vader_lexicon_path
@@ -44,13 +50,12 @@ class CreateData:
 
         pytrends = TrendReq(hl='en-Uk', tz=360)
 
-        keywords = keywords
-        pytrends.build_payload(keywords, timeframe=timeframe, geo=geo)
+        pytrends.build_payload(self.keywords, timeframe=self.timeframe, geo=self.geo)
 
-        trends_data = pytrends.interest_over_time()
-        return pd.DataFrame(trends_data)
+        self.trends = pytrends.interest_over_time()
+        return pd.DataFrame(self.trends)
 
-    def merge_datasets(self, stock_data, trends):
+    def merge_datasets(self, stock_data):
         """
         Required input (param) and output (return) are
             :param stock_data: Stock price data from EIKON
@@ -58,7 +63,7 @@ class CreateData:
             :return: A merged dataset which has the stock price data along with trends search data and keeps the 'New Close Price' as the end column
         """
         # Resetting index for trends data so the date column is no longer used as the index and can instead be used to merge the datasets
-        new_trends = trends.reset_index()
+        new_trends = self.trends.reset_index()
 
         # Ensuring both date columns are in datetime format
         stock_data['Exchange Date'] = pd.to_datetime(stock_data['Exchange Date'])
@@ -74,14 +79,18 @@ class CreateData:
 
         # Adjusting the columns order to have 'Next Day Close' on the end
         columns = merged_nearest_df.columns.tolist()
-        columns[-1], columns[-2] = columns[-2], columns[-1]
+        columns.remove('Next Day Close')
+        columns.append('Next Day Close')
         merged_nearest_df = merged_nearest_df[columns]
 
         return merged_nearest_df
 
+    def return_data(self):
+        stocks = self.format_data()
+        self.get_trends()
+        return self.merge_datasets(stocks)
 
-formatted_data = CreateData().format_data(df)
-trends = CreateData().get_trends(['Astrazeneca', 'covid'], '2019-03-31 2024-03-27', 'GB')
 
-updated_df = CreateData().merge_datasets(formatted_data, trends)
+create_data = CreateData(data, ['Astrazeneca', 'covid'], '2019-03-31 2024-03-27', 'GB')
+updated_df = create_data.return_data()
 print(updated_df)
