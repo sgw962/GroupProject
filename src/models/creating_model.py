dee9a6d2bc876bffc984ca61063483a6b6a6db2a
Sean <seangerardwhite@gmail.com>
2024-05-14 19:12:41 +0100

    Added hyperparameter testing to model class

--- src/models/creating_model.py
+++ src/models/creating_model.py
@@ -5,7 +5,7 @@ import matplotlib.dates as mdates
 import openpyxl
 import seaborn as sns
 import numpy as np
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from pytrends.request import TrendReq
@@ -30,8 +30,10 @@ class CreateModel:
         self.model = None
         self.y_pred = None
 
-    def build_model(self):
-        self.model = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+    def build_model(self, params=None):
+        if params is None:
+            params = {'n_estimators': 100, 'learning_rate': 0.1, 'max_depth': 3, 'random_state': 42}
+        self.model = GradientBoostingRegressor(**params)
 
         # Fit the model
         self.model.fit(self.X_train, self.y_train)
@@ -76,6 +78,16 @@ class CreateModel:
             plt.tight_layout()
             plt.show()
 
+    def tune_parameters(self, param_grid):
+        grid_search = GridSearchCV(estimator=GradientBoostingRegressor(), param_grid=param_grid, scoring='neg_mean_squared_error', cv=5, n_jobs=-1, verbose=2)
+        grid_search.fit(self.X_train, self.y_train)
+
+        print("Best Parameters found: ", grid_search.best_params_)
+        print("Best CV Score: ", -grid_search.best_score_)
+
+        self.best_params_ = grid_search.best_params_
+        return self.best_params_
+
 
 # data = pd.read_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/EasyJet Price History.xlsx')
 
@@ -85,7 +97,19 @@ class CreateModel:
 # print(updated_df)
 #visualise_correlation(updated_df)
 data = pd.read_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/group_project_code/src/data/Ocado Stock & trends.xlsx')
+
+param_grid = {
+    'n_estimators': [50, 100, 200],
+    'learning_rate': [0.01, 0.1, 0.2],
+    'max_depth': [3, 4, 5],
+    'subsample': [0.8, 1.0]
+}
+
 create_model = CreateModel(data)
-#create_model.build_model()
+
+best_params = create_model.tune_parameters(param_grid)
+print("Best Hyperparameters:", best_params)
+
+create_model.build_model(best_params)
 create_model.plot_model()
 create_model.feature_importance()
