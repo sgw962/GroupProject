e8b5d03f6f201cc7035c842abf26c4ff48fb806f
Sean <seangerardwhite@gmail.com>
2024-06-06 17:28:13 +0100

    Updated hyperparameters

--- src/models/creating_model.py
+++ src/models/creating_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV
+from sklearn.model_selection import train_test_split, GridSearchCV, RandomizedSearchCV, cross_val_score
 from sklearn.metrics import make_scorer, mean_squared_error, r2_score, mean_absolute_error, mean_absolute_percentage_error
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import accuracy_score
@@ -34,7 +34,12 @@ class CreateModel:
 
     If wanting to save the metrics as a table in Excel call get_metrics_dataframe().
     """
+
     def __init__(self, df):
+        interaction = (df.iloc[:, 6] + df.iloc[:, 7] + df.iloc[:, 8]) / 3
+        df.insert(len(data.columns) - 1, 'Trends', interaction)
+        columns_to_drop = df.columns[6:9]
+        df.drop(columns=columns_to_drop, inplace=True)
         self.df = df
         self.df['Exchange Date'] = pd.to_datetime(self.df['Exchange Date'])
         self.df_sorted = self.df.sort_values(by='Exchange Date')
@@ -69,11 +74,11 @@ class CreateModel:
         val_data = self.df_sorted.iloc[test_end_idx:]
 
         # Define features and target
-        self.X_train = train_data.iloc[:, 1:9]
+        self.X_train = train_data.iloc[:, 1:7]
         self.y_train = train_data.iloc[:, -1]
-        self.X_test = test_data.iloc[:, 1:9]
+        self.X_test = test_data.iloc[:, 1:7]
         self.y_test = test_data.iloc[:, -1]
-        self.X_val = val_data.iloc[:, 1:9]
+        self.X_val = val_data.iloc[:, 1:7]
         self.y_val = val_data.iloc[:, -1]
 
         self.test_dates = test_data['Exchange Date']
@@ -83,6 +88,19 @@ class CreateModel:
         print("Testing data length:", len(test_data))
         print("Validation data length:", len(val_data))
 
+    def build_model(self, parameters=None, training_x=None, training_y=None, testing_x=None):
+        if training_x is None:
+            raise Exception("Please provide training data")
+
+        if parameters is None:
+            parameters = {'updater': 'coord_descent', 'n_estimators': 1000, 'learning_rate': 0.3, 'lambda': 0.0001,
+                          'feature_selector': 'greedy', 'booster': 'gblinear', 'alpha': 0,
+                          'objective': 'reg:squarederror'}
+        self.model = XGBRegressor(**parameters)
+        self.model.fit(training_x, training_y)
+        predicted_y = self.model.predict(testing_x)
+        return predicted_y
+
     def evaluate_model(self, y_true, y_pred, set_name="Test Set"):
         mse = mean_squared_error(y_true, y_pred)
         rmse = np.sqrt(mse)
@@ -90,58 +108,9 @@ class CreateModel:
         mae = mean_absolute_error(y_true, y_pred)
         mape = mean_absolute_percentage_error(y_true, y_pred)
 
-        self.metrics[set_name] = {
-            'R2': r2,
-            'MSE': mse,
-            'RMSE': rmse,
-            'MAE': mae,
-            'MAPE': mape
-        }
+        self.metrics[set_name] = {'R2': r2, 'MSE': mse, 'RMSE': rmse, 'MAE': mae, 'MAPE': mape}
 
         print(f'{set_name} - \nMean Squared Error: {mse}\nRoot Mean Squared Error: {rmse}\nR^2 Score: {r2}\nMean Absolute Error: {mae}\nMean Absolute Percentage Error: {mape}%')
-
-    def train_model(self, params=None):
-        if params is None:
-            params = {'booster': 'gblinear', 'feature_selector': 'greedy', 'updater': 'coord_descent'}
-        self.model = XGBRegressor(**params)
-
-        # Fit the model
-        self.model.fit(self.X_train, self.y_train)
-
-        # Predict the Close prices
-        self.y_pred = self.model.predict(self.X_test)
-        self.y_val_pred = self.model.predict(self.X_val)
-
-        if np.any(self.y_test == 0):
-            print("Warning: There are zero values in the true labels, which can cause high MAPE.")
-
-        # Evaluate the model on the test set
-        self.evaluate_model(self.y_test, self.y_pred, '\nTest Set')
-
-        # Evaluate the model on the validation set
-        self.evaluate_model(self.y_val, self.y_val_pred, '\nValidation Set')
-
-        self.line_plot('Test Data', self.test_dates, self.y_test, self.y_pred)
-        self.line_plot('Validation Data', self.val_dates, self.y_val, self.y_val_pred)
-
-        self.feature_importance('X Train Set')
-
-    def get_metrics_dataframe(self):
-        return pd.DataFrame(self.metrics).transpose()
-
-    def scatter_plot(self):
-        if self.y_pred is None:
-            print('Cannot plot model as model has not been built yet. Please call build_model() first.')
-            return
-        else:
-            plt.figure(figsize=(8, 6))
-            plt.scatter(self.y_test, self.y_pred, alpha=0.5)
-            plt.title('Actual vs. Predicted Values')
-            plt.xlabel('Actual Values')
-            plt.ylabel('Predicted Values')
-            plt.plot([self.y_test.min(), self.y_test.max()], [self.y_test.min(), self.y_test.max()], 'k--', lw=2)  # Line showing perfect predictions
-            plt.grid(True)
-            plt.show()
 
     def line_plot(self, data_name, time_frame, actual, predicted):
         plt.plot(time_frame, predicted, label='Predicted Values')
@@ -153,6 +122,15 @@ class CreateModel:
         plt.legend()
         plt.show()
 
+    def scatter_plot(self, test_y, pred_y):
+        plt.figure(figsize=(8, 6))
+        plt.scatter(test_y, pred_y, alpha=0.5)
+        plt.title('Actual vs. Predicted Values')
+        plt.xlabel('Actual Values')
+        plt.ylabel('Predicted Values')
+        plt.plot([test_y.min(), test_y.max()], [test_y.min(), test_y.max()], 'k--', lw=2)
+        plt.show()
+
     def feature_importance(self, features_name):
         if self.model is None:
             print('Cannot show feature importance as model has not been built yet. Please call build_model() first.')
@@ -161,7 +139,11 @@ class CreateModel:
             feature_names = self.df.drop(['Exchange Date', 'Next Day Close'], axis=1).columns
             importances = self.model.feature_importances_
 
-            print('Feature Importances:', importances)
+            #print('\nFeature Importances:', importances)
+            feature_importances = dict(zip(feature_names, importances))
+            print('\nFeature Importances:')
+            for feature, importance in feature_importances.items():
+                print(f'{feature}: {importance}')
             if np.any(importances < 0):
                 print('Warning: Some feature importances are negative')
 
@@ -179,9 +161,10 @@ class CreateModel:
             'MSE': make_scorer(mean_squared_error, greater_is_better=False),
             'MAE': make_scorer(mean_absolute_error, greater_is_better=False),
             'MAPE': make_scorer(mean_absolute_percentage_error, greater_is_better=False)
-                }
+        }
 
-        grid_search = GridSearchCV(estimator=XGBRegressor(), param_grid=params, scoring=scorers, cv=3, n_jobs=-1, verbose=1, refit='R2', error_score='raise')#, n_iter=300)
+        grid_search = RandomizedSearchCV(estimator=XGBRegressor(), param_distributions=params, scoring=scorers, cv=5,
+                                         n_jobs=-1, verbose=1, refit='R2', error_score='raise', n_iter=150)
         grid_search.fit(self.X_train, self.y_train)
 
         print("Best Parameters found: ", grid_search.best_params_)
@@ -190,55 +173,62 @@ class CreateModel:
         self.best_params_ = grid_search.best_params_
         return self.best_params_
 
-    def retrain_with_validation(self, params=None):
-        if params is None:
-            params = {'booster': 'gblinear', 'feature_selector': 'greedy', 'updater': 'coord_descent'}
-        self.model = XGBRegressor(**params)
+    def train_model(self, params=None):
+        self.y_pred = self.build_model(params, self.X_train, self.y_train, self.X_test)
+        self.evaluate_model(self.y_test, self.y_pred, '\nTest Set')
 
-        # Combine training and validation data
+        self.y_val_pred = self.build_model(params, self.X_train, self.y_train, self.X_val)
+        self.evaluate_model(self.y_val, self.y_val_pred, '\nValidation Set')
+
+        self.line_plot('Test Data', self.test_dates, self.y_test, self.y_pred)
+        self.line_plot('Validation Data', self.val_dates, self.y_val, self.y_val_pred)
+
+        self.scatter_plot(self.y_test, self.y_pred)
+        self.scatter_plot(self.y_val, self.y_val_pred)
+
+        self.feature_importance('X Train Set')
+
+    def retrain_with_validation(self, params=None):
         X_combined = pd.concat([self.X_train, self.X_val])
         y_combined = pd.concat([self.y_train, self.y_val])
 
-        # Fit the model
-        self.model.fit(X_combined, y_combined)
-
-        # Predict the Close prices
-        self.y_pred = self.model.predict(self.X_test)
-
-        # Evaluate the model on the test set
+        self.y_pred = self.build_model(params, X_combined, y_combined, self.X_test)
         self.evaluate_model(self.y_test, self.y_pred, "\nTest Set After Retraining with Validation")
 
         # Evaluate the model on the validation set
-        self.y_val_pred = self.model.predict(self.X_val)
+        self.y_val_pred = self.build_model(params, X_combined, y_combined, self.X_val)
         self.evaluate_model(self.y_val, self.y_val_pred, "\nValidation Set After Retraining with Validation")
 
         self.line_plot('Test Data After Retraining with Validation', self.test_dates, self.y_test, self.y_pred)
         self.line_plot('Validation Data After Retraining with Validation', self.val_dates, self.y_val, self.y_val_pred)
 
+        self.scatter_plot(self.y_test, self.y_pred)
+        self.scatter_plot(self.y_val, self.y_val_pred)
+
         self.feature_importance('X Train with Validation Set')
 
     def retrain_without_trends(self, params=None):
-        if params is None:
-            params = {'booster': 'gblinear', 'feature_selector': 'greedy', 'updater': 'coord_descent'}
-        self.model = XGBRegressor(**params)
-
-        columns_to_drop = [5, 6, 7]
+        columns_to_drop = [5]
         new_train = self.X_train.drop(self.X_train.columns[columns_to_drop], axis=1)
         new_test = self.X_test.drop(self.X_test.columns[columns_to_drop], axis=1)
         new_val = self.X_val.drop(self.X_val.columns[columns_to_drop], axis=1)
 
-        self.model.fit(new_train, self.y_train)
-        new_y_pred = self.model.predict(new_test)
+        self.y_pred = self.build_model(params, new_train, self.y_train, new_test)
+        self.evaluate_model(self.y_test, self.y_pred, '\nTest Set Without Trends Data')
 
-        self.evaluate_model(self.y_test, new_y_pred, '\nTest Set Without Trends Data')
+        self.y_val_pred = self.build_model(params, new_train, self.y_train, new_val)
+        self.evaluate_model(self.y_val, self.y_val_pred, '\nValidation Set Without Trends Data')
 
-        new_y_val_pred = self.model.predict(new_val)
-        self.evaluate_model(self.y_val, new_y_val_pred, '\nValidation Set Without Trends Data')
+        self.line_plot('Test Set Without Trends Data', self.test_dates, self.y_test, self.y_pred)
+        self.line_plot('Validation Data Without Trends Data', self.val_dates, self.y_val, self.y_val_pred)
 
-        self.line_plot('Test Set Without Trends Data', self.test_dates, self.y_test, new_y_pred)
-        self.line_plot('Validation Data Without Trends Data', self.val_dates, self.y_val, new_y_val_pred)
+        self.scatter_plot(self.y_test, self.y_pred)
+        self.scatter_plot(self.y_val, self.y_val_pred)
 
         self.feature_importance('X Train Set without Trends Data')
+
+    def get_metrics_dataframe(self):
+        return pd.DataFrame(self.metrics).transpose()
 
 
 param_grid = {
@@ -249,8 +239,8 @@ param_grid = {
     'alpha': [0, 0.000001, 0.0001, 0.01, 0.1, 1],  # L1 regularization term on weights
     'feature_selector': ['thrifty', 'cyclic', 'shuffle', 'random', 'greedy'],  # Feature selector type
     'updater': ['coord_descent'],# 'shotgun'],  # Updater type for linear booster
-    'objective': ['reg:squaredlogerror', 'reg:squarederror', ],  # Learning objective
-}
+    'objective': ['reg:squaredlogerror', 'reg:squarederror', ]  # Learning objective
+            }
 
 params_list = {
     #'alpha': 0.1,
@@ -263,18 +253,13 @@ params_list = {
     'updater': 'coord_descent'
     }
 
-boosters = {
-    'booster': ['gblinear', 'gbtree', 'dart']
-}
-
 data = pd.read_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/group_project_code/data/stocks & trends/Ocado Stock & trends.xlsx')
 
-plt.plot(data['Exchange Date'], data['Next Day Close'])
 
+plt.plot(data['Exchange Date'], data['Next Day Close'])
 plt.title('Next Day Close for whole Ocado Data')
 plt.xlabel('Exchange Date')
 plt.ylabel('Next Day Close Price')
-plt.legend()
 plt.show()
 
 
@@ -283,10 +268,9 @@ create_model.split_data(0.8, 0.1, 0.1)
 
 #best_params = create_model.tune_parameters(boosters)
 
-create_model.train_model(params_list)
-create_model.scatter_plot()
-create_model.retrain_with_validation(params_list)
-create_model.retrain_without_trends(params_list)
+create_model.train_model()
+create_model.retrain_with_validation()
+create_model.retrain_without_trends()
 metrics_df = create_model.get_metrics_dataframe()
 
 metrics_df.to_excel('/Users/seanwhite/OneDrive - University of Greenwich/Documents/Group Project/group_project_code/data/metrics tables/Ocado Metrics.xlsx')
